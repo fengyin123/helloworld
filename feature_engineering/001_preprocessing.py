eb069c23b7fdb06c0c2f3901810fa9baa26cde71
Huang Qiang <huangq2@fugue.org.aalto.fi>
2018-04-10 16:03:17 +0300

    001, 101

--- feature_engineering/001_preprocessing.py
+++ feature_engineering/001_preprocessing.py
@@ -1,127 +1,94 @@
-
-# coding: utf-8
-
-# In[12]:
-
-
-
-import pandas as pd
+# %load 101_user_based_statistics.py
+import gc
 import numpy as np
-import lightgbm as lgb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss
-from sklearn import preprocessing
-import warnings
-import datetime
+import pandas as pd
+from tqdm import tqdm
+from utils import BayesianSmoothing, load_pickle, dump_pickle, raw_data_path
 
-warnings.filterwarnings("ignore")
+train = pd.read_pickle('../data/train.pkl')
+test = pd.read_pickle('../data/test.pkl')
+df = pd.read_pickle('../data/df.pkl')
 
-import time
+train_feat = pd.DataFrame({'instance_id': train.instance_id})
+test_feat = pd.DataFrame({'instance_id': test.instance_id})
 
-from utils import raw_data_path, dump_pickle
+user_cols = ['user_id', 'user_gender_id', 'user_age_level', 'user_occupation_id', 'user_star_level']
+id_cols = ['instance_id', 'item_id', 'item_brand_id', 'item_city_id', 'shop_id']
+df_cols = df.columns
+# grade_cols = ['item_price_level', 'item_sales_level', 'item_collected_level', 'item_pv_level', 'shop_review_num_level',
+#               'shop_review_positive_rate', 'shop_star_level', 'shop_score_service', 'shop_score_delivery', 'shop_score_description']
+grade_cols = ['item_price_level']
 
-path = '../data/'
-train_file = 'round1_ijcai_18_train_20180301.txt'
-test_file = 'round1_ijcai_18_test_a_20180301.txt'
+def get_stats_target(df, group_column, target_column, drop_raw_col=False):
+    df_old = df.copy()
+    grouped = df_old.groupby(group_column)
+    the_stats = grouped[target_column].agg(['mean', 'median', 'max', 'min', 'std', 'skew']).reset_index()
+    the_stats.columns = [ group_column,
+                            '_%s_groupby_%s_mean' % (target_column, group_column),
+                            '_%s_groupby_%s_median' % (target_column, group_column),
+                            '_%s_groupby_%s__max' % (target_column, group_column),
+                            '_%s_groupby_%s__min' % (target_column, group_column),
+                            '_%s_groupby_%s__std' % (target_column, group_column),
+                            '_%s_groupby_%s__skew' % (target_column, group_column)
+                        ]
+    if drop_raw_col:
+        df_old.drop(group_column, axis=1, inplace=True)
 
-# def load_data():
-#     train = pd.read_table(path + train_file, encoding='utf8', delim_whitespace=True)
-#     test = pd.read_table(path + train_file, encoding='utf8', delim_whitespace=True)
-#     df = pd.concat([train, test], axis=0, ignore_index=True)
+    return the_stats
 
-def date_convert(data):
-    # Transform into datetime format
-    data['time'] = pd.to_datetime(data.context_timestamp, unit='s')
+def generate_basic_feats(df, feat_df):
 
-    # transform into Beijing datetime format
-    data['realtime'] = data['time'].apply(lambda x: x + datetime.timedelta(hours=8))
-    data['day'] = data['realtime'].dt.day
-    data['hour'] = data['realtime'].dt.hour
-    
-    return data
-
-def base_process(data):
-    lbl = preprocessing.LabelEncoder()
-    print("========================item==========================")
-    # Divided into different category levels and LabelEncoder()
+    # ============================= 计算关于用户特征的id类特征的数量 =====================
     '''
-    item_id, item_category_list, item_property_list, item_brand_id, item_city_id, 
-    item_price_level, item_sales_level, item_collected_level, item_pv_level
+    如：对于每个男性用户, 他所浏览的各种item_id的数量
     '''
-    for i in range(1, 3):
-        data['item_category_list' + str(i)] = lbl.fit_transform(data['item_category_list'].map(
-            lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else 'missing'))
-    del data['item_category_list'] 
-        
-    # for i in range(10):
-    #     data['item_property_list' + str(i)] = lbl.fit_transform(data['item_property_list'].map(
-    #         lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else ''))
-    # del data['item_property_list']
-    data_item_property = data['item_property_list'].str.split(';', expand=True).add_prefix('item_property_')
-    data_item_property.fillna('missing', inplace=True)
-    # train_item_property = lbl.fit_transform(train_item_property)
-    # data = pd.concat([data, train_item_property], axis=1)
+    for user_col in tqdm(user_cols):
+        cnt_result = df.groupby(user_col)[id_cols].nunique()
+        cnt_result = cnt_result.add_prefix(user_col + '_').add_suffix('_cnt')
+        cnt_result = cnt_result.reset_index()
+        feat_df[user_col + '_count'] = pd.merge(df, cnt_result, on=user_col, how='left').iloc[:, -1].values
 
-    for col in ['item_id', 'item_brand_id', 'item_city_id']:
-        data[col] = lbl.fit_transform(data[col])
-    
-    # Fill none with mean
-    data['item_sales_level'][data.item_sales_level==-1] = None
-    data['item_sales_level'].fillna(data['item_sales_level'].mean(), inplace=True)
-    
-    
-    print("========================user==========================")
-    # user_gender_id and user_occupation_id should be handled with one-hot
-    data[data.user_age_level==-1]['user_age_level'] = None
-    data['user_age_level'].fillna(data['user_age_level'].mode())
-    data['user_age_level'] = data['user_age_level'].apply(lambda x: x%1000)
-    
-    data[data.user_star_level==-1]['user_star_level'] = None
-    data['user_star_level'].fillna(data['user_star_level'].mean())
-    data['user_star_level'] = data['user_star_level'].apply(lambda x: x%3000)
-    
-    
-    print("=====================context==========================")
-    data = date_convert(data)
-    
-    for i in range(5):
-        data['predict_category_property' + str(i)] = lbl.fit_transform(data['predict_category_property'].map(
-            lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else ''))
-    del data['predict_category_property'] 
-        
-    print("=====================shop===============================")
-    data['shop_score_service'][data.shop_score_service==-1] = None
-    data['shop_score_service'].fillna(data['shop_score_service'].mean(), inplace=True)
-    
-    data['user_age_level'][data.user_age_level==-1] = None
-    data['shop_score_delivery'].fillna(data['shop_score_delivery'].mean(), inplace=True)
-    
-    data['shop_score_description'][data.shop_score_description==-1] = None
-    data['shop_score_description'].fillna(data['shop_score_description'].mean(), inplace=True)
-    
-    return data
-    
+    # ============================ 计算关于用户特征的得分类特征的统计特征 ========================
+    '''
+    如：男性用户中item_price_level的平均数，中位数，最大值，最小值
+    '''
+    for user_col in tqdm(user_cols):
+        # statistic feature
+        for grade_col in tqdm(grade_cols):
+            the_stats = get_stats_target(df, user_col, grade_col, drop_raw_col=False)
+            #stats_list = pd.concat([stats_list, the_stats], axis=1)
+            tmp_df = pd.merge(df, the_stats, on=user_col)
+            feat_df = pd.concat([feat_df, tmp_df.drop(df_cols, axis=1)] , axis=1)
 
-if __name__ == "__main__":
-    start = time.time()
-    print("Load Data")
-    train = pd.read_table(path + train_file, encoding='utf8', delim_whitespace=True)
-    test = pd.read_table(path + test_file, encoding='utf8', delim_whitespace=True)
-    len_train = train.shape[0]
-    df = pd.concat([train, test], axis=0, ignore_index=True)
-    print("Start doing preprocessing")
-    df = base_process(df)
-    dump_pickle(df, path=raw_data_path + 'df.pkl')
-    print(df.day.unique())
-    train = df[df['day']<=24]
-    # train = df[(df['day'] >= 18) & (df['day'] <= 23)]
-    # valid = df[(df['day'] == 24)]
-    dump_pickle(train, path=raw_data_path + 'train.pkl')
-    # dump_pickle(valid, path=raw_data_path + 'valid.pkl')
+    # ========================== 计算每个用户下的category2的个数 =========================
+    '''
+    如：对于每个男性用户，他所浏览的各种category2的数量
+    '''
+    for user_col in tqdm(user_cols):
+        # category and property columns
+        category_cnt = df.groupby(user_col)['item_category_list2'].nunique().reset_index()
+        category_cnt.columns = [user_col, 'category2_groupby_{}_cnt'.format(user_col)]
+        tmp_df = pd.merge(df, category_cnt, how='left', on=user_col)
+        feat_df = pd.concat([feat_df, tmp_df.drop(df_cols, axis=1)], axis=1)
 
-    test = df.iloc[len_train:]
-    dump_pickle(test, path=raw_data_path + 'test.pkl')
+        del category_cnt
+        gc.collect()
 
-    end = time.time()
-    print("Preprocessing done and time elapsed %s" % (end-start))
+    return feat_df
+    # # ======================== 计算每个用户特征下分别有多少用户 ===================================
+    # '''
+    #     如：对于每个男性用户，他所浏览的各种category2的数量
+    # '''
+    # for user_col in tqdm(user_cols):
+    #     if user_col == 'user_id':
+    #         continue
+    #     else:
+    #         tmp_groupby_cnt = category_df.groupby(user_col)['user_id'].nunique().reset_index()
+    #         tmp_groupby_cnt.columns = [user_col, 'id_groupby_{}_cnt'.format(user_col)]
+    #         df = pd.merge([df, tmp_groupby_cnt], how='left', on=user_col)
 
+train_feat = generate_basic_feats(train, train_feat)
+test_feat = generate_basic_feats(test, test_feat)
+
+dump_pickle(train, raw_data_path + 'train/' + '101_user_based_statistics.pkl')
+dump_pickle(test, raw_data_path + 'test/' + '101_user_based_statistics.pkl')
